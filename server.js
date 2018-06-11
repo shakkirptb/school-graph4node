806b84d122ec3a34f1a1fddaa5b4ee832edbc619
Muhammed Shakkir Thalikunnath <shakkirptb@gmail.com>
2018-06-11 18:04:29 +0530

    new query testing added

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var Graph = require("graph4node");
 
 //read from file or db 
-//*order of data is important
+//order of data is important*
 var rawNodeData = JSON.parse(fs.readFileSync('./lib/node-data.json', 'utf8'));
 
 //Graph
@@ -14,18 +14,18 @@ var schoolGraph = new Graph(rawNodeData);
 
 /**
  * query1:
- * find all students from physics course who scored more than 70
+ * find all students from economics course who scored more than 70
  * */
 var query1 = schoolGraph.execute({
     select: "*",
-    from: ["student", "physics"],
+    from: ["student", "economics"],
     where: {
         marks: {
             $gt: 70
         }
     }
 });
-console.log("query-result-1", query1.getShallow());
+console.log("query (all students from economics course who scored more than 70):\n", Graph.getShallow(query1));
 
 /**
  * query2:
@@ -41,4 +41,29 @@ var query2 = schoolGraph.execute({
         }
     }
 });
-console.log("query-result-2", query2.getShallow());
+console.log("query (all students who scored between 60 and 70):\n", Graph.getShallow(query2));
+
+/** 
+ * query3:
+ * sum of marks in engineering
+ * */
+var query3 = schoolGraph.execute({
+	select: {$sum:"marks"},
+	from: ["student","engineering"],
+});
+console.log("sum of marks in engineering:\n", Graph.getShallow(query3));
+
+/** 
+ * query4:
+ * count of students in engineering
+ * */
+var query4 = schoolGraph.execute({
+	select: "$count",
+	from: ["student","engineering"],
+});
+console.log("count of students in engineering:\n", Graph.getShallow(query4));
+
+
+
+
+
